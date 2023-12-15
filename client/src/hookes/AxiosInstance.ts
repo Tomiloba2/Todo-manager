4276e4becb7421d6dc8c91e8e372b98d3a422862
TOMILOBA <oluwatomilobaomojola@gmail.com>
2023-12-14 19:02:01 -0800

    added vite env

--- client/src/hookes/AxiosInstance.ts
+++ client/src/hookes/AxiosInstance.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const instance=axios.create({
-    withCredentials:true,
-    baseURL:"http://localhost:2020/api/"
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: import.meta.env.VITE_BASEURL
 })
 
 export default instance
